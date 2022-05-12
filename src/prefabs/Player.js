50923be8a2bd53065b2a7eaf367452dca7655441
Michelle Kim <mkim139@ucsc.edu>
2022-05-11 18:27:12 -0700

    pushing before matthew jumps the gun

---
+++ src/prefabs/Player.js
@@ -0,0 +1,13 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+      super(scene, x, y, texture);
+
+      scene.physics.add.existing(this);
+      scene.add.existing(this);
+
+    }
+
+
+    update() {
+      
+}  
--- src/scenes/Store.js
+++ src/scenes/Store.js
@@ -1,3 +1,5 @@
+//store
+
 class Store extends Phaser.Scene {
     constructor() {
         super("storeScene");
@@ -7,17 +9,29 @@ class Store extends Phaser.Scene {
         this.load.image("store_bg", "./assets/shop/shop_bg.png");
         this.load.image("cart", "./assets/shop/test_cart.png");
         this.load.image("shelf", "./assets/shop/test_shelf.png");
-        //this.load.image("cerealBox", "./assets/shop/text_box.png");
+        this.load.image("cerealBox", "./assets/shop/test_box.png");
+
+        this.load.image("collider", "./assets/shop/collider.png");
     }
 
     create() {
+        //test assets
         this.bg = this.add.tileSprite(0,0, 980, 720, "store_bg").setOrigin(0,0);
-        //this.bg = this.add.image(0,0,"store_bg");
+        //this.shelf = this.add.sprite(0, 15, "shelf").setOrigin(0,0);
+
+        let collider = this.physics.add.sprite(0, 150, "collider").setOrigin(0,0);
+        collider.body.allowGravity = false;
+        collider.setImmovable();
+        let ground = this.physics.add.sprite(0, game.config.height - 50, "collider").setOrigin(0,0);
+        ground.body.allowGravity = false;
+        ground.setImmovable();
+
+        player = new Player(this.physics.add.sprite(0, 0, "cart").setOrigin(0,0));
     }
 
     update() {
         if(pointer.isDown){
-            this.scene.start('cartScene');
+            //this.scene.start('cartScene');
         }
     }
 }
